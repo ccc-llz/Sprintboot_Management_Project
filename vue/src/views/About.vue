8994c1193b2d9891b3462c0ea1facd1b669036d7
ccc_llz <chiale1123@gmail.com>
2025-07-18 11:17:15 +1000

    construct the primitive framework and setting css

---
+++ vue/src/views/About.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "About"
+})
+</script>
+
+<template>
+  <div>About</div>
+</template>
+
+<style scoped>
+
+</style>
