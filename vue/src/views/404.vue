a2ec87fb94840f1b451144201e513aa9e3f92750
ccc_llz <chiale1123@gmail.com>
2025-07-18 19:10:16 +1000

    import element-plus and edit buttons and themecolor

---
+++ vue/src/views/404.vue
@@ -0,0 +1,13 @@
+
+<template>
+<div style="height: 100vh; display: flex; flex-direction: column;align-items: center; justify-content: center">
+  <div style="width: 50%">
+  <img style="width: 100%" src="@/assets/imgs/404.png" alt="">
+  </div>
+  <div style="padding: 20px 0; font-size: 20px" ><a style="color: cornflowerblue" href="/">redirect to Main page</a></div>
+</div>
+
+</template>
+
+<script lang="ts">
+</script>
